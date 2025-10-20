58b001a95cb6da598aa1bd5ba01feb2c0fd997e0
Recep Tayyip Karmış <rtkikarmis@gmail.com>
2025-10-20 20:50:17 +0300

    Ürünler ve ürün kategorilerinde page speed güncellemesi yapıldı.

--- client/corporate/src/app/su-aritma-urunleri/ProductContent.tsx
+++ client/corporate/src/app/su-aritma-urunleri/ProductContent.tsx
@@ -5,39 +5,35 @@ import InfoCard from "@/components/ui/InfoCard";
 import PageTitle from "@/components/ui/PageTitle";
 import { productCategories } from "@/data/products";
 import { buildProductsMainBreadcrumb } from "@/lib/breadcrumbs";
-import { motion } from "framer-motion";
-import React from "react";
 
 export default function ProductContent() {
   return (
     <div>
-      <motion.section
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-      >
+      <section>
         {productCategories.length > 0 && (
           <div>
             <Breadcrumb items={buildProductsMainBreadcrumb()} />
             <PageTitle text="Ürünlerimiz" />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {productCategories.map((cat) => (
-                <React.Fragment key={cat.slug}>
-                  <article className="bg-gray-50 rounded-2xl shadow hover:shadow-lg transition-all p-6">
-                    <InfoCard
-                      imageUrl={`/images/products/categories/${cat.imageName}`}
-                      title={cat.name}
-                      description={cat.desc}
-                      ctaUrl={`/su-aritma-urunleri/${cat.slug}`}
-                    />
-                  </article>
-                </React.Fragment>
+            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              {productCategories.map((cat, i) => (
+                <li
+                  key={cat.slug}
+                  className="p-6 bg-gray-50 rounded-2xl shadow-sm md:hover:shadow-lg md:transition-all duration-300"
+                >
+                  <InfoCard
+                    imageUrl={`/images/products/categories/${cat.imageName}`}
+                    title={cat.name}
+                    description={cat.desc}
+                    ctaUrl={`/su-aritma-urunleri/${cat.slug}`}
+                    loading={i === 0 ? "eager" : "lazy"}
+                  />
+                </li>
               ))}
-            </div>
+            </ul>
           </div>
         )}
-      </motion.section>
+      </section>
     </div>
   );
 }
--- client/corporate/src/app/su-aritma-urunleri/[kategori]/ProductCategoryContent.tsx
+++ client/corporate/src/app/su-aritma-urunleri/[kategori]/ProductCategoryContent.tsx
@@ -5,8 +5,6 @@ import InfoCard from "@/components/ui/InfoCard";
 import PageTitle from "@/components/ui/PageTitle";
 import { buildProductCategoryBreadcrumb } from "@/lib/breadcrumbs";
 import { Product, ProductCategory } from "@/types/product";
-import { motion } from "framer-motion";
-import React from "react";
 
 export default function ProductCategoryContent({
   category,
@@ -19,35 +17,32 @@ export default function ProductCategoryContent({
 }) {
   return (
     <div>
-      <motion.section
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-      >
+      <section>
         <div>
           <Breadcrumb items={buildProductCategoryBreadcrumb(category)} />
           <PageTitle text={category.name} />
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {products.map((product) => (
-              <React.Fragment key={product.slug}>
+          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {products.map((product, i) => (
+              <li
+                key={product.slug}
+                className="p-6 bg-gray-50 rounded-2xl shadow-sm md:hover:shadow-lg md:transition-all duration-300"
+              >
                 <SchemaProductSingle product={product} />
-                <article className="bg-gray-50 rounded-2xl shadow hover:shadow-lg transition-all p-6">
-                  <InfoCard
-                    imageUrl={
-                      product.colorVariants.find((v) => v.isCover)
-                        ?.imageList[0] || ""
-                    }
-                    title={product.name}
-                    description={product.shortDesc}
-                    ctaUrl={`/su-aritma-urunleri/${params.kategori}/${product.slug}`}
-                  />
-                </article>
-              </React.Fragment>
+                <InfoCard
+                  imageUrl={
+                    product.colorVariants.find((v) => v.isCover)
+                      ?.imageList[0] || ""
+                  }
+                  title={product.name}
+                  description={product.shortDesc}
+                  ctaUrl={`/su-aritma-urunleri/${params.kategori}/${product.slug}`}
+                  loading={i === 0 ? "eager" : "lazy"}
+                />
+              </li>
             ))}
-          </div>
+          </ul>
         </div>
-      </motion.section>
+      </section>
     </div>
   );
 }
